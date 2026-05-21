f5980ed43bc0000efe6167b8d53a0b38cfa971a8
svetlana2794 <sb270994@gmail.com>
2026-05-21 13:49:51 +0300

    cors

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express"
+import cors from "cors"
 
 import { catalog } from "./data/data.js"
 
 const PORT=process.env.PORT || 3000
 const app=express()
+
+app.use(cors({
+origin: 'https://shopnode.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+  credentials: true      
+}))
 
 app.use(express.urlencoded({
 limit: "50mb",
@@ -24,6 +32,26 @@ res.send(ans)
 app.get("/api/catalog", (req, res) => {
 	
 res.json(catalog)
+})
+
+
+
+
+
+function filterCatalog(req, array) {
+
+let at=array.filter((elem) => {
+if (elem.price>=req.query.at)
+return {
+id: elem.id,
+name: elem.name,
+promo: elem.promo,
+price: elem.price,
+color: elem.color,
+img: elem.img,
+hit: elem.hit,
+newly: elem.newly
+}
 })
 
 app.listen(PORT, () => console.log("server running"))
